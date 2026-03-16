b8bfc259849cf29aec0e1ae703744d2c7af61cc2
pheemma <eaa29101975@gmail.com>
2026-03-16 15:05:23 +0300

    Worked project

--- client.py
+++ client.py
@@ -2,11 +2,12 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
+sock.setblocking(1)
+sock.connect(('localhost', 9090))
+
+msg = input("Input Text")
+#msg = "Hi!"
 sock.send(msg.encode())
 
 data = sock.recv(1024)
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 
 sock = socket.socket()
+# Source - https://stackoverflow.com/a/6380198
+# Posted by Bryan, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-03-16, License - CC BY-SA 3.0
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 sock.bind(('', 9090))
 sock.listen(0)
 conn, addr = sock.accept()
